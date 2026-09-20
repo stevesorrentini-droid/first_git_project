d57f3d827655f3a64b879d1243dde977ae5b4269
stevesorrentini-droid <stevesorrentini@gmail.com>
2026-09-20 12:26:23 -0400

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,6 +3,8 @@ package day01;
 public class HelloWorld {
     static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Cydeo!");
+        System.out.println("Hello Programmers!");
 
     }
 }
