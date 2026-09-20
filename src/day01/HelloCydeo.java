0086ae21970357b0042fbdae7da155a1c8fb6e07
stevesorrentini-droid <stevesorrentini@gmail.com>
2026-09-20 12:32:47 -0400

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,6 +4,6 @@ public class HelloCydeo {
     static void main(String[] args) {
         System.out.println("Hello Cydeo!");
 
-        // some changes are add
+        // some changes are added
     }
 }
